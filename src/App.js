2be2fceb4c2f1edde1ad5db8b78efe0f4455866d
SherzodAxadov2 <sherzodaxadov69@gmail.com>
2023-02-13 20:47:40 +0500

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./App.css";
 import React from "react";
 import Home from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import "./navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [navList, setNavList] = useState([
+    { id: 1, name: "Главная", href: "home", isActive: true },
+    { id: 2, name: "Создать свой флаер", href: "create", isActive: false },
+    { id: 3, name: "О нас", href: "about", isActive: false },
+    { id: 4, name: "Наши работы", href: "works", isActive: false },
+    { id: 5, name: "Обратная связь", href: "contact", isActive: false },
+  ]);
+
+  const getStatus = (id) => {
+    setNavList(
+      navList.map((list) =>
+        list.id === id
+          ? { ...list, isActive: true }
+          : { ...list, isActive: false }
+      )
+    );
+  };
+
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="navbar-wrapper">
+          <a href="#home" className="nav-logo">
+            <img
+              src="https://flaer.uz/img/flayer.jpg"
+              alt="logo"
+              style={{ width: "75px", height: "24px" }}
+            />
+          </a>
+
+          <ul className="nav-list">
+            {navList.map((nav) => {
+              return (
+                <li className="nav-item" key={nav.id}>
+                  <a
+                    href={`#${nav.href}`}
+                    className={nav.isActive ? "active" : ""}
+                    onClick={() => getStatus(nav.id)}
+                  >
+                    {nav.name}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
