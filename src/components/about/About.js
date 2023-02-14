458074638a12277a431cb5134186cd5df4cf687d
oktam <oktamgapurjanov@gmail.com>
2023-02-14 18:24:52 +0500

    about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,24 @@
 // O'ktam
 import React from "react"
 import "./about.css"
-const About =() =>{
-    return(
+const About = () => {
+    return (
         <div className="about" id="about">
-            
+            <div className="containder">
+                <h1>O нас</h1>
+                <p>
+                    B нашем конструкторе флаеров Вы можете создать свой собственный неповторимый дизайн флаера.
+                </p>
+                <div className="main-box">
+                    <div className="left-img">
+                        <img src="https://flaer.uz/img/markus-spiske-144806-unsplash.jpg" alt="" />
+                    </div>
+                    <div className="right-text">
+                        <h3>
+                            ЧТО TAKOE ФЛАЕР?</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
